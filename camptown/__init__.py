354df938f04a78b2800f4f5415cc547301b5f469
fluffy <fluffy@beesbuzz.biz>
2023-11-03 20:16:01 -0700

    Release v0.3.2

--- camptown/__init__.py
+++ camptown/__init__.py
@@ -1,13 +1,13 @@
 """ Build the Camptown player """
+import collections
 import functools
 import logging
 import os
 import os.path
+import re
 import shutil
 import typing
 from urllib.parse import urlparse
-import collections
-import re
 
 import jinja2
 import mistune
@@ -51,18 +51,20 @@ def lyrics(text):
 
 FileCallback = typing.Callable[[str], str]
 
-class UniqueSlugify:
-    """ A slugifier that maintains output filename uniqueness """
-    def __init__(self):
-        self.counts:typing.Dict[str,int] = collections.defaultdict(lambda:0)
 
-    def __call__(self, text):
+def slugify():
+    """ Slugify a filename that maintains uniqueness in case of collision """
+    counts: typing.Dict[str, int] = collections.defaultdict(lambda: 0)
+
+    def _slugify(text):
         slug = re.sub(r'[^0-9a-zA-Z._]+', '-', text)
-        count = self.counts[slug]
-        self.counts[slug] += 1
+        count = counts[slug]
+        counts[slug] += 1
         if count:
             slug += f'-{count}'
         return slug
+
+    return _slugify
 
 
 class InfoRenderer(mistune.HTMLRenderer):
@@ -73,7 +75,7 @@ class InfoRenderer(mistune.HTMLRenderer):
         super().__init__()
         self.output_dir = output_dir
         self.file_callback = file_callback
-        self.slugify = UniqueSlugify()
+        self.slugify = slugify()
         self.outfiles = outfiles
         self.map_cache: dict[str, str] = {}
 
