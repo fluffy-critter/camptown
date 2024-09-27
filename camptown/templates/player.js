e3d8bf5cbcb0b766865117f8e17f2a95b31ce5ff
fluffy <fluffy@beesbuzz.biz>
2024-09-27 03:46:25 -0700

    Switch to modal dialog and fix fullsize art setting

--- camptown/templates/player.js
+++ camptown/templates/player.js
@@ -43,7 +43,6 @@ window.addEventListener('load', () => {
 
     let fullSizeArt = document.getElementById('fullsizeart')
 
-console.log("1");
     document.getElementById('openfullsize').addEventListener('click', (e) => {
         e.stopPropagation();
 
@@ -51,18 +50,20 @@ console.log("1");
         if (fullSizeArt.open) {
             fullSizeArt.close();
         } else {
-            fullSizeArt.show();
+            fullSizeArt.showModal();
         }
     });
 
-console.log("2");
-    document.querySelector('dialog#fullsizeart').addEventListener('click', (e) => {
+    var dlg = document.querySelector('dialog#fullsizeart');
+    dlg.addEventListener('click', (e) => {
         e.stopPropagation();
 
         fullSizeArt.close();
     });
+    dlg.addEventListener('cancel', (e) => {
+        console.log("Dialog was canceled");
+    });
 
-console.log("3");
     // preserve the default album art
     var albumArt = document.querySelector('img#coverart');
     albumArt = {
@@ -70,14 +71,13 @@ console.log("3");
         srcset: albumArt.srcset,
         dataset: albumArt.dataset
     };
-console.log("4");
 
     // set the album art from what's now playing
     function setCoverArt(ref) {
         let thumb = document.querySelector('img#coverart');
+        console.log("setting artwork", ref, ref.dataset.fullsize);
         thumb.src = ref.src;
         thumb.srcset = ref.srcset;
-        console.log("setting artwork", thumb.src, thumb.srcset);
 
         document.querySelector('#fullsizeart img').src = ref.dataset.fullsize ?? albumArt.dataset.fullsize ?? '';
     }
