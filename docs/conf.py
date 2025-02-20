b294844048fb5ed5ceaca06b9b291c531dda650f
fluffy <fluffy@beesbuzz.biz>
2025-02-20 12:00:50 -0800

    Still trying to fix RTD

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-import sphinx_rtd_theme
-
 project = "camptown"
 master_doc = "index"
 extensions = ["sphinx.ext.autodoc"]
@@ -10,8 +8,5 @@ autoclass_content = "both"
 autodoc_member_order = "groupwise"
 autodoc_inherit_docstrings = False
 html_logo = "logo.png"
-
-
-html_theme = 'sphinx_rtd_theme'
 
 sys.path.insert(0, os.path.abspath('..'))
