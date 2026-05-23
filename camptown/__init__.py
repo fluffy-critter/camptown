baa9c57177e68bbf313bc8d3daf24464a48527b3
fluffy <fluffy@beesbuzz.biz>
2026-05-22 23:06:57 -0700

    Use importlib for version number for distributed builds

--- camptown/__init__.py
+++ camptown/__init__.py
@@ -17,7 +17,8 @@ from markupsafe import Markup, escape
 try:
     from .__version__ import __version__
 except ImportError:
-    __version__ = '(unknown)'
+    import importlib.metadata
+    __version__ = importlib.metadata.version('camptown')
 
 
 LOGGER = logging.getLogger(__name__)
